4d99529880b5dbd244ea59e2a1f067058e8e6beb
Garrett Marquez <gmarquez@planittesting.com>
2018-08-06 13:43:22 +1200

    added Search step definition class

---
+++ pom.specflow/Steps/SearchSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace pom.specflow.Steps
+{
+    [Binding]
+    public class SearchSteps
+    {
+        [Given(@"Planit Website is open")]
+        public void GivenPlanitWebsiteIsOpen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I search for something")]
+        public void WhenISearchForSomething()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be displayed")]
+        public void ThenTheResultShouldBeDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
