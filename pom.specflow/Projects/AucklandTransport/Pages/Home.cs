5350ef9c9a6d3fce9e42565b8e5c9bab87d715d1
Garrett Marquez <gmarquez@planittesting.com>
2018-08-21 09:29:00 +1200

    Hooks.scenario.CreateNode<When>(scenariocontext.StepContext.StepInfo.Text).Fail($"Failed to enter credentials due to error encountered in {e.Message}");

--- pom.specflow/Projects/AucklandTransport/Pages/Home.cs
+++ pom.specflow/Projects/AucklandTransport/Pages/Home.cs
@@ -6,7 +6,7 @@ namespace pom.specflow.Projects.AucklandTransport.Pages
     public class Home : Specflow.BasePage
     {
         protected readonly IWebDriver driver;
-        private By btn_Login = By.XPath("//a[contains(@href,'signin') and text()='Log inn']");
+        private By btn_Login = By.XPath("//a[contains(@href,'signin') and text()='Log in']");
         public Home(IWebDriver wd)
         {
             this.driver = wd;
